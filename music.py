4524cfafe608154f8e3abd6a4fcae89e283d7273
magicaz <952066338@qq.com>
2022-02-22 23:29:15 +0800

    First upload

--- music.py
+++ music.py
@@ -1,263 +1,91 @@
 import os
-import random
+import shutil
 import librosa
-import matplotlib.pyplot as plt
-import librosa.display
 
 
 class Music:
-    def __init__(self, project_path, sr):
-        print("--创建工程--")
-        print("当前工程目录：", project_path)
-        self.project_path = project_path
-        self.sr = sr
-        self.ogg_path = os.path.join(project_path, "base.ogg")
-        self.wav_path = os.path.join(project_path, "base.wav")
-
-        if not os.path.exists(self.wav_path):
-            print("未发现wav文件，ffpemg转换中...")
-            os.system("ffmpeg.exe -i %s %s" % (self.ogg_path, self.wav_path))
-
-    def load(self):
-        print("--加载音乐--")
-        print("采样率：%d" % self.sr)
+    def __init__(self, music_file):
+        """
+        创建音乐工程
+        :param project_path:
+        :param sr:
+        """
+        print("[添加音乐文件]")
+        self.file_path = music_file
+        self.file_name = os.path.basename(self.file_path).split(".")[0]
+        self.wav_path = self.format_convert("wav")
+        self.sr = librosa.get_samplerate(self.wav_path)
         self.audio, self.sr = librosa.load(self.wav_path, sr=self.sr)
+        self.duration = librosa.get_duration(self.audio, sr=self.sr)
+        self.get_bpm_and_offset()
+        print("-->音乐文件添加成功\nSampleRate:%s,Bpm:%s,Offset:%s." % (self.sr, self.bpm, self.offset))
 
-    def get_bpm(self):
-        print("--BPM计算--")
-        self.bpm, self.beats = librosa.beat.beat_track(self.audio, self.sr)
+    def format_convert(self, target_format):
+        print("[文件格式转换,目标格式：%s]" % target_format)
+        self.format_path = os.path.join(os.path.dirname(self.file_path), "base.%s" % target_format)
+        if not os.path.exists(self.format_path):
+            print("-->未发现%s文件,转换中..." % target_format)
+            try:
+                os.system("ffmpeg.exe -i %s %s" % (self.format_path, self.wav_path))
+            except Exception as e:
+                print(e)
+                print("-->格式转换失败")
+                return False
+
+        print("-->格式转换成功")
+        return self.format_path
+
+    def get_bpm_and_offset(self):
+        print("[BPM与OFFSET测定]")
+        self.bpm, self.beats = librosa.beat.beat_track(y=self.audio, sr=self.sr)
+        self.bpm = round(self.bpm)
+
         while self.bpm < 100:
-            print(">BPM小于100，扩大1倍<")
             self.bpm = round(self.bpm * 2)
-        print("BPM:", self.bpm)
+        while self.bpm > 400:
+            self.bpm = round(self.bpm / 2)
+        self.onset = librosa.frames_to_time(frames=librosa.onset.onset_detect(y=self.audio[0:5000]), sr=self.sr) * 1000
+        self.offset = round(self.onset[0])
 
-    def get_start(self):
-        print("--计算音乐开始点--")
-        self.onset_timestamps = librosa.frames_to_time(librosa.onset.onset_detect(self.audio), self.sr) * 1000
-        self.offset = round(self.onset_timestamps[0])
-        print("Offset:", self.offset)
+        # self.duration = librosa.get_duration(self.audio, self.sr) * 1000
+        # print("Duration:", self.duration)
 
-    def get_timestamps(self):
-        print("--生成时间戳--")
-        self.timestamps_delta = 60 * 1000 / self.bpm / 4
-        self.timestamps_delta_d4 = self.timestamps_delta * 4
-        print("时间戳增量（16分）：", round(self.timestamps_delta))
-        print("时间戳增量（4分）：", round(self.timestamps_delta_d4))
-        max_add_count = round(self.onset_timestamps[-1] / self.timestamps_delta) + 64
-        self.timestamps = [round(self.offset + i * self.timestamps_delta) for i in range(0, max_add_count)]
-        self.timestamps_d4 = [round(self.offset + i * self.timestamps_delta_d4) for i in range(0, round(max_add_count / 4))]
-        print("共生成%d个时间戳，开始时间：%d，结束时间：%d。" % (max_add_count, self.timestamps[0], self.timestamps[-1]))
+    def separate(self, stem_num):
+        print("[Spleeter拆分音乐,拆分数量:%d]" % stem_num)
+        # 5stems安装包可能下载有问题，需要手动下载解压后放在pretrianed_models目录下
+        self.separate_save_dir = os.path.join(os.path.dirname(self.file_path), "spleeter_separate_%d" % stem_num)
+        if not os.path.exists(self.separate_save_dir):
+            os.mkdir(self.separate_save_dir)
+            separate_cmd = "spleeter separate -o %s -p spleeter:%dstems %s" % (self.separate_save_dir, stem_num, self.file_path)
+            os.system(separate_cmd)
 
-    def onset_compare(self):
-        print("--Onset对齐--")
-        self.onset_timestamps_align = []
-        for ts in self.timestamps:
-            for ots in self.onset_timestamps:
-                if abs(ts - ots) < self.timestamps_delta:
-                    self.onset_timestamps_align.append(ts)
+    def reload(self, stems=2):
+        """
+        重加载音乐文件
+        """
+        self.separate(stem_num=stems)
+        print("[重加载音乐，用于分声部检测]")
+        self.diff_parts = []
+        for dif_part in ["vocals", "accompaniment", "drums", "bass", "other", "piano"]:
+            dir_part_path = os.path.join(self.separate_save_dir, self.file_name, dif_part + ".wav")
+            if os.path.exists(dir_part_path):
+                sepa_part = SeparatorPart(part=dif_part, path=dir_part_path, base_music=self)
+                self.diff_parts.append(sepa_part)
 
-    def beats_compare(self):
-        print("--Beats对齐--")
-        self.beats_timestamps = librosa.frames_to_time(self.beats, self.sr) * 1000
-        self.beats_timestamps_align = []
-        for ts in self.timestamps_d4:
-            for bts in self.beats_timestamps:
-                if abs(ts - bts) < self.timestamps_delta:
-                    self.beats_timestamps_align.append(ts)
+        print("-->加载完毕，共加载%d个分声部文件" % len(self.diff_parts))
 
 
-class Aff:
-    def __init__(self, bpm, offset):
-        self.bpm = bpm
-        self.offset = offset
-        self.aff = "AudioOffset:%d\n-\ntiming(0,%.2f,4.00);\n" % (self.offset, self.bpm)
-        self.objects = []
+class SeparatorPart:
+    def __init__(self, part, path, base_music):
+        self.part = part
+        self.path = path
+        self.base_music = base_music
+        self.sr = librosa.get_samplerate(self.path)
+        self.audio, self.sr = librosa.load(self.path, sr=self.sr)
 
-    def all_to_tap(self, tap_timestamps):
-        print(">>>时间戳转为轨道随机的tap...")
-        for o in tap_timestamps:
-            new_tap = Tap(track=random.randint(1, 4), time=o - self.offset)
-            self.add_object(new_tap)
+        self.onset_detect()
 
-    def beats_and_onset_to_tap(self, beats_timestamps, onset_timestamps):
-        self.all_to_tap(beats_timestamps)
-        for ots in onset_timestamps:
-            if ots not in beats_timestamps:
-                new_tap = Tap(track=random.randint(1, 4), time=ots - self.offset)
-                self.add_object(new_tap)
-
-        self.objects.sort(key=lambda x: x.start_time)
-
-    def tap_diff_track(self):
-        print(">>>连续同轨的tap乱序...")
-        current_track = 0
-        same_time = 0
-        for o in self.objects:
-            if current_track == o.track:
-                same_time += 1
-            else:
-                same_time = 0
-
-            if same_time > 2:
-                o.track = track_diff(o.track)
-
-            current_track = o.track
-
-    def tap_to_hold(self, time_interval):
-        print(">>>间隔较大的tap转为hold...")
-        object_length = len(self.objects)
-        for i in range(0, object_length - 1):
-            object_before = self.objects[i]
-            object_after = self.objects[i + 1]
-
-            if object_after.start_time - object_before.start_time > time_interval * 1.5:
-                self.objects[i] = Hold(track=random.randint(1, 4), start_time=object_before.start_time,
-                                       end_time=object_before.start_time + time_interval)
-
-    def add_object(self, insert_obj):
-        if insert_obj.type == "tap":
-            for exist_obj in self.objects:
-                if exist_obj.type == "tap" and insert_obj.start_time == exist_obj.start_time:
-                    return False
-                if exist_obj.type == "hold" and exist_obj.track == insert_obj.track and \
-                        exist_obj.start_time < insert_obj.start_time < exist_obj.end_time:
-                    return False
-
-        self.objects.append(insert_obj)
-        return True
-
-    def error_fix_hold(self):
-        error = 0
-        error = 0
-        for i in range(0, len(self.objects)):
-            if self.objects[i].type == "hold":
-                hold = self.objects[i]
-                for exist_obj in self.objects:
-                    if hold.track == exist_obj.track:
-                        if exist_obj.type == "hold" and (hold.start_time < exist_obj.start_time < hold.end_time or
-                                                         hold.start_time < exist_obj.end_time < hold.end_time):
-                            self.objects[i].track = track_diff(self.objects[i].track)
-                            error += 1
-
-        print(">>>hold问题修正,共%d处错误" % error)
-        return error
-
-    def error_fix_tap(self):
-        error = 0
-        for i in range(0, len(self.objects)):
-            if self.objects[i].type == "tap":
-                tap = self.objects[i]
-                for exist_obj in self.objects:
-                    if tap.track == exist_obj.track:
-                        if exist_obj.type == "hold" and exist_obj.start_time < tap.start_time < exist_obj.end_time:
-                            self.objects[i].track = track_diff(self.objects[i].track)
-                            error += 1
-
-        print(">>>tap问题修正,共%d处错误" % error)
-        return error
-
-    def random_hold_to_arc(self):
-        print(">>>hold随机转换为arc")
-        for i in range(0, len(self.objects)):
-            if self.objects[i].type == "hold" and random.randint(0, 1):
-                self.objects[i] = Arc(start_time=self.objects[i].start_time,
-                                      end_time=self.objects[i].end_time,
-                                      start_x=random.randint(0, 1),
-                                      end_x=random.randint(0, 100) / 100,
-                                      arc_type=random.choice(["s", "si", "so", "sisi", "siso", "sosi", "soso"]),
-                                      start_y=1,
-                                      end_y=random.randint(0, 100) / 100,
-                                      color=random.randint(0, 1),
-                                      param="none",
-                                      black="false")
-
-                self.objects[i].color = self.objects[i].start_x
-
-    def to_aff(self):
-        print(">>>转为aff文件,当前队列物件数：%d..." % len(self.objects))
-        objects_type_dict = {"tap": 0, "hold": 0, "arc": 0}
-        for o in self.objects:
-            objects_type_dict[o.type] += 1
-            self.aff = self.aff + o.get_aff() + "\n"
-        print(">>>转换完毕，其中Tap:%d|Hold:%d|Arc:%d。" % (objects_type_dict["tap"], objects_type_dict["hold"], objects_type_dict["arc"]))
-
-
-class Tap:
-    def __init__(self, track, time):
-        self.type = "tap"
-        self.track = track
-        self.start_time = time
-
-    def get_aff(self):
-        self.aff = "(%d,%d);" % (self.start_time, self.track)
-        return self.aff
-
-
-class Hold:
-    def __init__(self, track, start_time, end_time):
-        self.type = "hold"
-        self.track = track
-        self.start_time = start_time
-        self.end_time = end_time
-
-    def get_aff(self):
-        self.aff = "hold(%d,%d,%d);" % (self.start_time, self.end_time, self.track)
-        return self.aff
-
-
-class Arc:
-    # arc(1028,1371,0.25,0.25,s,0.00,0.00,0,none,true);
-    def __init__(self, start_time, end_time, start_x, end_x, arc_type, start_y, end_y, color, param, black):
-        self.type = "arc"
-        self.start_time = start_time
-        self.end_time = end_time
-        self.start_x = start_x
-        self.start_y = start_y
-        self.arc_type = arc_type
-        self.end_x = end_x
-        self.end_y = end_y
-        self.color = color
-        self.param = param
-        self.black = black
-
-    def get_aff(self):
-        self.aff = "arc(%d,%d,%.2f,%.2f,%s,%.2f,%.2f,%d,%s,%s);" \
-                   % (self.start_time, self.end_time, self.start_x, self.end_x, self.arc_type, self.start_y, self.end_y, self.color, self.param,
-                      self.black)
-        return self.aff
-
-
-def track_diff(track):
-    new_track = track
-
-    while new_track == track:
-        new_track = random.randint(1, 4)
-
-    # print("track:%d->%d" % (track, new_track))
-    return new_track
-
-
-if __name__ == '__main__':
-    project_path = "chart/worldexecuteme"
-    m = Music(project_path, 44100)
-    m.load()
-    m.get_bpm()
-    m.get_start()
-    a = Aff(m.bpm, m.offset)
-    m.get_timestamps()
-    m.onset_compare()
-    m.beats_compare()
-    a.beats_and_onset_to_tap(m.beats_timestamps_align, m.onset_timestamps_align)
-    a.tap_diff_track()
-    a.tap_to_hold(time_interval=m.timestamps_delta_d4)
-
-    tap_error = 1
-    hold_error = 1
-    while tap_error + hold_error > 0:
-        tap_error = a.error_fix_tap()
-        hold_error = a.error_fix_hold()
-    a.random_hold_to_arc()
-    a.to_aff()
-
-    with open(os.path.join(project_path, "3.aff"), "w", encoding="utf-8") as aff_file:
-        aff_file.write(a.aff)
+    def onset_detect(self):
+        print("-->%s声部正在进行音符端点检测" % self.part)
+        self.onset = librosa.frames_to_time(librosa.onset.onset_detect(self.audio), self.sr) * 1000
+        print("-->%s声部检测完毕,端点音符数:%d" % (self.part,len(self.onset)))
---
+++ project.py
@@ -0,0 +1,118 @@
+import os.path
+import random
+
+from arcfutil import aff
+from music import Music
+
+
+class Project:
+    def __init__(self, project_path):
+        """
+        :param project_path: 工程目录
+        """
+        self.project_path = project_path
+        self.afflist = []
+
+    def add_music(self, music_file, separate_stems):
+        self.music = Music(music_file=music_file)
+        self.music.reload(stems=separate_stems)
+
+    def get_timestamps(self, timestamps_div=4, tolerance_times=1):
+        """
+        :param timestamps_div: 时间戳分度值，以小节分拍为单位，默认值为4，按四分采音
+        :param tolerance_times: 可容忍最大距离，以时间戳增量为单位，默认值为1，为一个时间戳增量的长度,此值越大采音越多
+        """
+        print("[生成时间戳]")
+        if not self.music:
+            print("!Error:请添加音乐")
+            return
+
+        self.timestamps_div = timestamps_div
+        self.tolerance_times = tolerance_times
+
+        print("-->输入音频BPM:%d,输入音频偏置:%d,输入音频时长:%d,设定时间增量分度:%d]"
+              % (self.music.bpm, self.music.offset, self.music.duration * 1000, self.timestamps_div))
+        self.timestamps_delta = 60 * 1000 / self.music.bpm * (4 / self.timestamps_div)
+        print("-->时间戳增量：", round(self.timestamps_delta))
+        self.tolerance = round(self.timestamps_delta * self.tolerance_times)
+        print("-->容忍误差最大范围：", self.tolerance)
+        max_add_count = round(self.music.duration * 1000 / self.timestamps_delta)
+        self.timestamps = [round(self.music.offset + i * self.timestamps_delta) for i in range(0, max_add_count)]
+        print("-->共生成%d个时间戳，开始时间:%d，结束时间:%d。" % (max_add_count, self.timestamps[0], self.timestamps[-1]))
+
+        # 添加起始为0的小节线
+        self.afflist.append(aff.Timing(0, self.music.bpm))
+        # 添加符合延迟的小节线
+        self.afflist.append(aff.Timing(self.music.offset, self.music.bpm))
+
+    def segment_fusion(self, weight):
+        weight_sum = 0
+        for w in weight:
+            weight_sum += weight[w]
+        for w in weight:
+            weight[w] = weight[w] / weight_sum
+
+        print("-->归一化权重:", weight)
+
+        diff_part_onset_aligned = {}
+        for dp in self.music.diff_parts:
+            diff_part_onset_aligned[dp.part] = self.timestamps_align(base_ts=self.timestamps, input_ts=dp.onset)
+
+        self.timestamps_fusion = []
+        aligned_result = {}
+        for t in self.timestamps:
+            aligned_result[t] = 0
+        for oa in diff_part_onset_aligned:
+            if oa in weight:
+                for timestamp in diff_part_onset_aligned[oa]:
+                    aligned_result[timestamp] += weight[oa]
+
+        for art in aligned_result:
+            if aligned_result[art] >= 0.5:
+                self.timestamps_fusion.append(art)
+
+        print("-->融合完毕，共合并得到%d个有效时间戳" % (len(self.timestamps_fusion)))
+
+    def generate_notes(self, last_side=random.randint(0, 1), overlap=False):
+        """
+        按交互生成4K谱面,不指定起手则随机起手
+        """
+        if last_side == 0:
+            # 左起手
+            last_lane = 1
+        else:
+            # 右起手
+            last_lane = 4
+
+        for t in self.timestamps_fusion:
+            if last_side == 0:
+                # 向右侧布键
+                if overlap:
+                    next_lane = random.randint(last_lane, 4)
+                else:
+                    next_lane = random.randint(last_lane + 1, 4)
+                new_note = aff.Tap(time=t, lane=next_lane)
+            else:
+                # 向左侧布键
+                if overlap:
+                    next_lane = random.randint(1, last_lane)
+                else:
+                    next_lane = random.randint(1, last_lane - 1)
+                new_note = aff.Tap(time=t, lane=next_lane)
+
+            last_side = 1 - last_side
+            last_lane = next_lane
+
+            self.afflist.append(new_note)
+
+    def timestamps_align(self, base_ts, input_ts):
+        aligned_bts = []
+        for bts in base_ts:
+            for tts in input_ts:
+                if abs(bts - tts) < self.tolerance and bts not in aligned_bts:
+                    aligned_bts.append(bts)
+
+        return aligned_bts
+
+    def dumps(self, save_name):
+        aff.dumps(aff.AffList(self.afflist), os.path.join(self.project_path, save_name))
